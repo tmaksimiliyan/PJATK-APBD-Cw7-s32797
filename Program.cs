9ebfdc4f0ec65d2d7df22fd8fa0c0099f6c8d02a
maksymilian <maximustar2006@gmail.com>
2026-05-17 14:13:25 +0200

    chore: add Swagger for API testing

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using Cwiczenia7.Data;
+using Cwiczenia7.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +7,23 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddScoped<IPcService, PcService>();
+
 builder.Services.AddControllers();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
